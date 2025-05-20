046a0ba92d1d4f0ee7e56eef7ab90bd51578f089
amanpatel3045 <amanpatel3045@gmail.com>
2025-05-20 23:56:58 +0530

    created router for index file in app.js

---
+++ payment-app/src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+  
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+        </Routes>
+      </BrowserRouter>
+  
+  </QueryClientProvider>
+);
+
+export default App;
